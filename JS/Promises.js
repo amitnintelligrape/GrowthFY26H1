b55cf0efdc58fac4e22c0ad2257198e290dee3a8
Amit Narayan <amitnarayan@Amits-MacBook-Pro-4.local>
2025-06-10 18:07:59 +0530

    added async await Promises examples

--- JS/Promises.js
+++ JS/Promises.js
@@ -27,3 +27,44 @@ getApiData('https://dummyjson.com/test')
     .catch(error => {
       console.error('Custom Promise rejected with error:', error);
     });
+
+//async await example for fetching data
+const getProductData = async () => {  
+    const res = await fetch(`https://dummyjson.com/products`);
+    const data = await res.json();
+    console.log(data);
+};
+getProductData();
+
+const promise1 = new Promise((resolve) =>
+    setTimeout(() => resolve("Promise 1 resolved"), 1000)
+);
+const promise2 = new Promise((resolve) =>
+    setTimeout(() => resolve("Promise 2 resolved"), 500)
+);
+const promise3 = new Promise((resolve) =>
+    setTimeout(() => resolve("Promise 3 resolved"), 800)
+);
+  
+const promisesArray = [promise1, promise2, promise3];
+  
+Promise.all(promisesArray)
+.then((results) => {
+      console.log("All promises resolved:", results);
+})
+.catch((error) => {
+      console.error("At least one promise rejected:", error);
+});
+
+Promise.allSettled(promisesArray)
+.then((results) => {
+      console.log("All promises Setteled:", results);
+})
+
+Promise.race(promisesArray)
+.then((results) => {
+      console.log("First promise resolved:", results);
+})
+.catch((error) => {
+    console.error("At least one promise rejected:", error);
+});
